28a4de4ba63c2d641ea047419253536137e8365b
Russ Perry <rperry1886@gmail.com>
2023-09-19 22:44:49 -0400

    Fix: Social Links open in a new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,31 @@ import '../component-styles/footer-styles.css';
 function Footer() {
   return (
     <footer>
-      <a href="https://twitter.com/russintech" className="social-link">
+      <a
+        href="https://twitter.com/russintech"
+        className="social-link"
+        target="_blank"
+      >
         <i class="fa-brands fa-square-x-twitter"></i>
       </a>
-      <a href="https://www.instagram.com/snapsbyrp/" className="social-link">
+      <a
+        href="https://www.instagram.com/snapsbyrp/"
+        className="social-link"
+        target="_blank"
+      >
         <i class="fa-brands fa-square-instagram"></i>
       </a>
-      <a href="https://github.com/rperry99" className="social-link">
+      <a
+        href="https://github.com/rperry99"
+        className="social-link"
+        target="_blank"
+      >
         <i class="fa-brands fa-square-github"></i>
       </a>
       <a
         href="https://www.linkedin.com/in/russ-perry-22b638a8/"
         className="social-link"
+        target="_blank"
       >
         <i class="fa-brands fa-linkedin"></i>
       </a>
