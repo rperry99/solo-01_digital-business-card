ef52fba6689eb67987ea0ac972d11cac38082025
Russ Perry <rperry1886@gmail.com>
2023-09-20 10:26:51 -0400

    Add: Headshot

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,11 @@
+import Headshot from './Headshot';
 import Footer from './Footer';
 import '../component-styles/card-styles.css';
 
 function Card() {
   return (
     <div className="card">
+      <Headshot />
       <Footer />
     </div>
   );
---
+++ src/components/Headshot.jsx
@@ -0,0 +1,7 @@
+import '../component-styles/headshot-styles.css';
+
+function Headshot() {
+  return <div className="img-container"></div>;
+}
+
+export default Headshot;
