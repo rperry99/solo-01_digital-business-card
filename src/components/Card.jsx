1383c825554d9786ea87bccd7298e94d9b8807da
Russ Perry <rperry1886@gmail.com>
2023-09-19 22:42:11 -0400

    Add: Card, Footer

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import Footer from './Footer';
+import '../component-styles/card-styles.css';
+
+function Card() {
+  return (
+    <div className="card">
+      <Footer />
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import '../component-styles/footer-styles.css';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://twitter.com/russintech" className="social-link">
+        <i class="fa-brands fa-square-x-twitter"></i>
+      </a>
+      <a href="https://www.instagram.com/snapsbyrp/" className="social-link">
+        <i class="fa-brands fa-square-instagram"></i>
+      </a>
+      <a href="https://github.com/rperry99" className="social-link">
+        <i class="fa-brands fa-square-github"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/russ-perry-22b638a8/"
+        className="social-link"
+      >
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
